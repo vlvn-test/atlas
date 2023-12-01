fc09f3e994f688ec18d0bd2874d41fefa8d78685
burakulusooyy <burakulusooy@gmail.com>
2023-12-01 09:05:51 +0300

    test-pr1

--- deneme00.py
+++ deneme00.py
@@ -11,7 +11,10 @@ as
 das
 d
 asd
-as
+asa
+da
+sd
+ass
 yazı="Selam herkese, burada bazı sayılar var. Bunlar: 1, 2, 3, 4, 5"
 dizi1=yazı.split()#Eğer parantez içerisine herhangi bir şey yazılmazsa bu durumda boşluğu baz alır.
 dizi2=yazı.split(",")#Parantez içerisine herhangi bir şey yazıldığında o karakteri ayraç olarak görür ve dizi oluşturur.
