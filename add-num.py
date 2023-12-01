58ee0362921f342d9c72a40ed3c011d242e9047e
burakulusooyy <burakulusooy@gmail.com>
2023-12-01 10:42:32 +0300

    farklı-pr8last

---
+++ add-num.py
@@ -0,0 +1,21 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+#sum2 = num1 + num2
+
+# Display the sum
+kasladj
+skdjfls
+şlksjdflks
+ooeakjrmlfkd
+lskjrflk
+
+şlksjdflks
+ooeakjrmlfkd
+
+2847683764912 şldskfş 872340982
+389274 lksdjf 3827497380
